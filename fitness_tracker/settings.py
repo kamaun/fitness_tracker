82e0fc7b66b8d9523840c2da5ad6e5866b00534a
Kelvin Njeri <knjeri@kinetixsolutions.com>
2020-06-07 16:53:21 -0400

    changed settings

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -87,6 +87,13 @@ DATABASES = {
     }
 }
 
+if config('ONSERVER', cast=bool):
+    import dj_database_url
+
+    prod_db = dj_database_url.config(default=config('DATABASE_URL'))
+    # prod_db = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(prod_db)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
